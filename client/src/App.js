9674e4141e0c295628e862b033ec27db1bc0cddd
wildan phidrai-ngam <wildan8376@gmail.com>
2024-02-06 22:55:06 +0700

    first-test commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import LoginPage from "./pages/LoginPage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
