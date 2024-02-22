62e42900b746615493faed77b373c4c333beba85
wildan phidrai-ngam <wildan8376@gmail.com>
2024-02-22 23:12:23 +0700

    using AuthContext in course-detail, (send state into addtocartbutton components)

--- client/src/components/student/course-detail.js
+++ client/src/components/student/course-detail.js
@@ -1,15 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import ax from "../../utils/config/ax";
 import conf from "../../utils/config/main";
 import { useParams } from "react-router-dom";
 import AddToCartButton from "./AddTocartButton";
+import { AuthContext } from "../../utils/auth/Auth.context";
 
 export default function CourseDetail() {
+  const { state } = useContext(AuthContext);
+
   const { courseName } = useParams();
   const [course, setCourse] = useState({});
-  const [isLoggedIn, setIsLoggedIn] = useState(false); 
-  const [role, setRole] = useState(''); 
-  
+
+
+
   const fetchItems = async () => {
     const response = await ax.get(
       `${conf.getCourseDetailEndpoint}${courseName}`
@@ -21,35 +24,98 @@ export default function CourseDetail() {
     fetchItems();
   }, []);
 
+
   return (
     <>
-      <h1 style={{ backgroundColor: "#004AAD", color: "white", textAlign: "center", padding: "20px", fontFamily: "ArchTH", fontSize: "30px", fontWeight: "bold" }}>วิชา {courseName}</h1>
+      <h1
+        style={{
+          backgroundColor: "#004AAD",
+          color: "white",
+          textAlign: "center",
+          padding: "20px",
+          fontFamily: "ArchTH",
+          fontSize: "30px",
+          fontWeight: "bold",
+        }}
+      >
+        วิชา {courseName}
+      </h1>
 
       <div style={{ display: "flex", alignItems: "center" }}>
-        <h1 style={{ backgroundColor: "#004AAD", color: "white", textAlign: "center", padding: "10px", fontFamily: "ArchTH", fontSize: "22px", fontWeight: "bold" }}>
-          คำอธิบายรายวิชา</h1>
+        <h1
+          style={{
+            backgroundColor: "#004AAD",
+            color: "white",
+            textAlign: "center",
+            padding: "10px",
+            fontFamily: "ArchTH",
+            fontSize: "22px",
+            fontWeight: "bold",
+          }}
+        >
+          คำอธิบายรายวิชา
+        </h1>
       </div>
       <div style={{ marginTop: "10px" }}>
         <div style={{ marginLeft: "50px" }}>
-          <p style={{ whiteSpace: "pre-line", fontFamily: "ArchTH", fontSize: "18px" }}>
-            {course.description}</p>
+          <p
+            style={{
+              whiteSpace: "pre-line",
+              fontFamily: "ArchTH",
+              fontSize: "18px",
+            }}
+          >
+            {course.description}
+          </p>
         </div>
       </div>
       <div style={{ display: "flex", alignItems: "center" }}>
-        <h1 style={{ backgroundColor: "#004AAD", color: "white", textAlign: "center", padding: "10px", fontFamily: "ArchTH", fontSize: "22px", fontWeight: "bold" }}>
-          พัฒนาวิชาโดย</h1>
+        <h1
+          style={{
+            backgroundColor: "#004AAD",
+            color: "white",
+            textAlign: "center",
+            padding: "10px",
+            fontFamily: "ArchTH",
+            fontSize: "22px",
+            fontWeight: "bold",
+          }}
+        >
+          พัฒนาวิชาโดย
+        </h1>
       </div>
       <div style={{ marginTop: "10px" }}>
         <div style={{ marginLeft: "50px" }}>
-          <p style={{ whiteSpace: "pre-line", fontFamily: "ArchTH", fontSize: "18px" }}>
-            {course.name_teacher}</p>
-          <p style={{ whiteSpace: "pre-line", fontFamily: "ArchTH", fontSize: "18px" }}>
-            {course.phone_number}</p>
-          <p style={{ whiteSpace: "pre-line", fontFamily: "ArchTH", fontSize: "18px" }}>
-            {course.mail_teacher}</p>
+          <p
+            style={{
+              whiteSpace: "pre-line",
+              fontFamily: "ArchTH",
+              fontSize: "18px",
+            }}
+          >
+            {course.name_teacher}
+          </p>
+          <p
+            style={{
+              whiteSpace: "pre-line",
+              fontFamily: "ArchTH",
+              fontSize: "18px",
+            }}
+          >
+            {course.phone_number}
+          </p>
+          <p
+            style={{
+              whiteSpace: "pre-line",
+              fontFamily: "ArchTH",
+              fontSize: "18px",
+            }}
+          >
+            {course.mail_teacher}
+          </p>
         </div>
       </div>
-      <AddToCartButton isLoggedIn={isLoggedIn} role={role} />
+      <AddToCartButton isLoggedIn={state?.isLoggedIn} role={state?.user?.role} />
     </>
   );
 }
--- client/src/pages/student/ProfilePage.js
+++ client/src/pages/student/ProfilePage.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import ax from "../../utils/config/ax";
 import conf from "../../utils/config/main";
-import { Container } from "react-bootstrap";
+import { Card, Container } from "react-bootstrap";
+
+import { Image } from "react-bootstrap";
 
 export default function ProfilePage() {
   const [userInfomation, setUserInfomation] = useState(null);
@@ -26,11 +28,24 @@ export default function ProfilePage() {
   if (userInfomation) {
     return (
       <Container>
-        <img
-          style={{ maxHeight: "100px", maxWidth: "100px" }}
-          src={"http://localhost:1337" + userInfomation.avatar.url}
-        />
-        <p>{userInfomation.username}</p>
+        <Card className="text-center mb-3">
+          <Card.Body>
+            <Card.Img
+              style={{
+                maxHeight: "100px",
+                maxWidth: "100px",
+                borderRadius: "50%",
+              }}
+              src={"http://localhost:1337" + userInfomation.avatar.url}
+            />
+            <p>{userInfomation.username}</p>
+            <h1>Email Address</h1>
+            <p>{userInfomation.email}</p>
+          </Card.Body>
+        </Card>
+        <Card>
+          <Card.Body>ประวัติการซื้อ</Card.Body>
+        </Card>
         <p>คอร์สทั้งหมด</p>
         {userInfomation.courses.map((course) => (
           <p key={course.id}>{course.name}</p>
