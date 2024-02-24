4239505d3de3a0aae3d087a7bf9c95073cf8eae3
wildan phidrai-ngam <wildan8376@gmail.com>
2024-02-24 12:55:38 +0700

    merge

--- client/src/pages/student/CourseDetailPage.js
+++ client/src/pages/student/CourseDetailPage.js
@@ -1,18 +1,36 @@
 import CourseDetail from "../../components/student/course-detail-page/course-detail";
 import AddToCartButton from "../../components/student/course-detail-page/AddTocartButton";
-import { useContext } from "react";
+
+import { Button, Container } from "react-bootstrap";
+
+import { useContext, useEffect } from "react";
 import { AuthContext } from "../../utils/auth/Auth.context";
+import conf from "../../utils/config/main";
+import ax from "../../utils/config/ax";
 
 export default function CourseDetailPage() {
   const { state } = useContext(AuthContext);
 
+  const fetchItems = async () => {
+    try {
+      const response = await ax.get(conf.getUserCourseEnrollments);
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+
   return (
-    <>
+    <Container>
       <CourseDetail />
-      <AddToCartButton
-        isLoggedIn={state?.isLoggedIn}
-        role={state?.user?.role}
-      />
-    </>
+      {state.isLoggedIn && state.user.role === "Student" && (
+        <Button variant="secondary"></Button>
+      )}
+    </Container>
   );
 }
--- client/src/utils/config/main.js
+++ client/src/utils/config/main.js
@@ -10,6 +10,7 @@ const conf = {
     "/categories?populate[courses][populate]=*&filters[name][$eq]=",
   getCourseDetailEndpoint: "/courses?populate=*&filters[name][$eq]=",
   getAllCourse: "/courses?populate=*",
+  getUserCourseEnrollments: "/users/me?populate[courses][populate]=*"
 };
 
 export default conf;
