00b3aef6a5608d41f7afe730b7b51842963db7a9
wildan phidrai-ngam <wildan8376@gmail.com>
2024-02-27 15:47:41 +0700

    commit

--- client/src/pages/student/StudyPage.js
+++ client/src/pages/student/StudyPage.js
@@ -1,30 +1,53 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import ax from "../../utils/config/ax";
 import conf from "../../utils/config/main";
 import { useParams } from "react-router-dom";
 import { Card, Container } from "react-bootstrap";
 import ReactPlayer from "react-player";
+import { AuthContext } from "../../utils/auth/Auth.context";
 
 export default function StudyPage() {
+  const { state } = useContext(AuthContext);
   const [materials, setMaterials] = useState([]);
   const [selectedMaterial, setSelectedMaterial] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [progression, setProgression] = useState(0);
 
   const { courseName } = useParams();
 
   const fetchItem = async () => {
     try {
       setIsLoading(true);
-      const response = await ax.get(
+
+      // Fetch materials
+      const materialsResponse = await ax.get(
         `${conf.getMaterialFilteredByCourseName}${courseName}`
       );
-      setMaterials(
-        response.data.data[0].attributes.materials.data
-          .map((material) => {
-            return { id: material.id, ...material.attributes };
-          })
-          .sort((a, b) => a.chapter_number - b.chapter_number)
+      const materialsData =
+        materialsResponse.data.data[0].attributes.materials.data.map(
+          (material) => ({ id: material.id, ...material.attributes })
+        );
+
+      // Fetch learning progresses
+      const userLearningProgressesResponse = await ax.get(
+        "/users/me?populate[learning_progresses][populate]=*"
       );
+      const learningProgressesData =
+        userLearningProgressesResponse.data.learning_progresses
+          .filter((progress) => progress.course.name === courseName)
+          .sort(
+            (a, b) => a.material.chapter_number - b.material.chapter_number
+          );
+
+      // Merge materials with learning progresses
+      const mergedData = materialsData.map((material) => {
+        const correspondingProgress = learningProgressesData.find(
+          (progress) => progress.material.id === material.id
+        );
+        return { ...material, progress: correspondingProgress };
+      });
+
+      setMaterials(mergedData);
     } catch (err) {
       console.log(err);
     } finally {
@@ -33,14 +56,102 @@ export default function StudyPage() {
   };
 
   const handleSelectMaterial = async (material) => {
-    setSelectedMaterial(material);
+    try {
+      if (!isLoading) {
+        setIsLoading(true);
+        setSelectedMaterial(material);
+      }
+      setSelectedMaterial(material);
+      var progress_response = await ax.get(
+        `/users/me?populate[learning_progresses][populate][material][filters][id][$eq]=${material.id}`
+      );
+    } catch (err) {
+      setIsLoading(false);
+      console.log(err);
+    } finally {
+      var is_has_progress = progress_response.data.learning_progresses.find(
+        (progress) => progress?.material?.id == material.id
+      );
+      if (!is_has_progress) {
+        try {
+          var post_progress_response = await ax.post(`/progresses`, {
+            data: {
+              material: { connect: [{ id: material.id }] },
+              progress: 0,
+              course: { connect: [{ id: material.course.data.id }] },
+              owner: { connect: [{ id: state.user.id }] },
+            },
+          });
+        } catch (err) {
+          console.log(err);
+        } finally {
+          // console.log(post_progress_response);
+        }
+      } else {
+        try {
+          var put_progress_response = await ax.put(
+            `/progresses/${is_has_progress.id}`,
+            {
+              data: {
+                progress: progression,
+              },
+            }
+          );
+        } catch (err) {
+          console.log(err);
+        } finally {
+          setIsLoading(false);
+          // console.log(put_progress_response);
+        }
+      }
+      setIsLoading(false);
+    }
   };
 
   const updateLearningProgress = async () => {
-    try {
-    } catch (err) {
-      console.log(err);
-    } finally {
+    if (selectedMaterial) {
+      try {
+        var progress_response = await ax.get(
+          `/users/me?populate[learning_progresses][populate][material][filters][id][$eq]=${selectedMaterial.id}`
+        );
+      } catch (err) {
+        console.log(err);
+      } finally {
+        var is_has_progress = progress_response.data.learning_progresses.find(
+          (progress) => progress?.material?.id == selectedMaterial.id
+        );
+        if (!is_has_progress) {
+          try {
+            var post_progress_response = await ax.post(`/progresses`, {
+              data: {
+                material: { connect: [{ id: selectedMaterial.id }] },
+                progress: 0,
+                course: { connect: [{ id: selectedMaterial.course.data.id }] },
+                owner: { connect: [{ id: state.user.id }] },
+              },
+            });
+          } catch (err) {
+            console.log(err);
+          } finally {
+            // console.log(post_progress_response);
+          }
+        } else {
+          try {
+            var put_progress_response = await ax.put(
+              `/progresses/${is_has_progress.id}`,
+              {
+                data: {
+                  progress: progression,
+                },
+              }
+            );
+          } catch (err) {
+            console.log(err);
+          } finally {
+            // console.log(put_progress_response);
+          }
+        }
+      }
     }
   };
 
@@ -48,10 +159,19 @@ export default function StudyPage() {
     fetchItem();
   }, []);
 
+  useEffect(() => {}, [materials]);
+
   useEffect(() => {
-    console.log(selectedMaterial);
-    fetchItem();
-  }, [selectedMaterial]);
+    // console.log(progression);
+  }, [progression]);
+
+  useEffect(() => {
+    updateLearningProgress();
+  }, [progression]);
+
+  useEffect(() => {
+    // console.log(materials);
+  }, [materials]);
 
   return (
     <Container>
@@ -62,7 +182,9 @@ export default function StudyPage() {
             selectedMaterial.video.data[0].attributes.url
           }
           controls
-          onProgress={() => console.log("test")}
+          onProgress={({ playedSeconds }) =>
+            setProgression(Math.round(playedSeconds))
+          }
           config={{ file: { attributes: { controlsList: "nodownload" } } }}
         ></ReactPlayer>
       )}
@@ -70,7 +192,10 @@ export default function StudyPage() {
         <Card
           className="text-center my-3"
           style={{ cursor: "pointer" }}
-          onClick={() => handleSelectMaterial(material)}
+          onClick={() => {
+            setSelectedMaterial(material);
+            handleSelectMaterial(material);
+          }}
           key={material.id}
         >
           <Card.Body>
@@ -78,6 +203,10 @@ export default function StudyPage() {
               บทที่ {material.chapter_number}: {material.title}
             </Card.Title>
             <Card.Subtitle>{material.description}</Card.Subtitle>
+            <Card.Text>
+              เรียนไปแล้ว{" "}
+              {material?.progresses?.data[0]?.attributes?.progress || 0} วินาที
+            </Card.Text>
           </Card.Body>
         </Card>
       ))}
