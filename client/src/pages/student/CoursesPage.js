8e74ea50780807eefcae785cb8d7c4d4659a5044
wildan phidrai-ngam <wildan8376@gmail.com>
2024-03-04 18:01:24 +0700

    courses page edited (remain pagination)

--- client/src/pages/student/CoursesPage.js
+++ client/src/pages/student/CoursesPage.js
@@ -2,9 +2,14 @@ import { useEffect, useState } from "react";
 import CourseList from "../../components/student/courses-page/course-list";
 import ax from "../../utils/config/ax";
 import conf from "../../utils/config/main";
+import CoursesPageHeader from "../../components/student/courses-page/courses-page-header";
 
 export default function CoursesPage() {
   const [courses, setCourses] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filteredCourses, setFilteredCourses] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategoriesId, setSelectedCategoriesId] = useState([]);
 
   const fetchItems = async () => {
     try {
@@ -17,14 +22,53 @@ export default function CoursesPage() {
           };
         })
       );
+      const categories_response = await ax.get("/categories?populate=courses");
+      setCategories(
+        categories_response.data.data.map((category) => {
+          return { id: category.id, ...category.attributes };
+        })
+      );
     } catch (err) {
       console.log(err);
     }
+  };
+
+  const handleSearch = () => {
+    setFilteredCourses(
+      courses.filter((course) => {
+        return search.toLowerCase() === ""
+          ? course
+          : course.name.toLowerCase().includes(search);
+      })
+    );
   };
 
   useEffect(() => {
     fetchItems();
   }, []);
 
-  return <CourseList courses={courses} />;
+  useEffect(() => {
+    setFilteredCourses(courses);
+  }, [courses]);
+
+  return (
+    <>
+      <CoursesPageHeader
+        setSearch={setSearch}
+        courses={courses}
+        setCourses={setCourses}
+        handleSearch={handleSearch}
+      />
+      <CourseList
+        courses={courses}
+        filteredCourses={filteredCourses}
+        setFilteredCourses={setFilteredCourses}
+        selectedCategoriesId={selectedCategoriesId}
+        setSelectedCategoriesId={setSelectedCategoriesId}
+        search={search}
+        categories={categories}
+      />
+      ;
+    </>
+  );
 }
