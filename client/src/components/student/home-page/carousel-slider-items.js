f1aa51bf850c215b3171b4ba44d7a62e99363310
Krit <jasada.k04@gmail.com>
2024-03-05 01:47:46 +0700

    fix(HomePage): dynamic marginTop when login and not login

--- client/src/components/student/home-page/carousel-slider-items.js
+++ client/src/components/student/home-page/carousel-slider-items.js
@@ -1,12 +1,22 @@
 import { Row, Col, Container, Image } from "react-bootstrap";
+import { AuthContext } from "../../../utils/auth/Auth.context";
+import React, { useContext, useState } from "react";
 
 export default function CarouselSliderItems() {
+  const { state: ContextState, login, logout } = useContext(AuthContext);
+
   return (
+    
     <Container>
       <Row>
-        <Image style={{maxHeight: "350px", zIndex: "-1 !important", marginTop :"80px"}} src="/www.skillplusplus.com.jpg" />
+        <Image
+          style={{
+            maxHeight: "350px",
+            zIndex: "-1 !important",
+            marginTop: !ContextState.isLoggedIn ? "135px" : "80px"
+          }}
+          src="/www.skillplusplus.com.jpg" />
       </Row>
     </Container>
   );
 }
-    
