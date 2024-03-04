4109fddb89b93cd0df16da1e509951428604a025
เนติวุฒิ เกตุกำพล <6610110542@psu.ac.th>
2024-03-05 02:23:58 +0700

    pull

--- client/src/components/student/home-page/carousel-slider-items.js
+++ client/src/components/student/home-page/carousel-slider-items.js
@@ -1,12 +1,22 @@
 import { Row, Col, Container, Image } from "react-bootstrap";
+import { AuthContext } from "../../../utils/auth/Auth.context";
+import React, { useContext,} from "react";
 
 export default function CarouselSliderItems() {
+  const { state: ContextState, login, logout } = useContext(AuthContext);
+
   return (
+    
     <Container>
       <Row>
-        <Image style={{maxHeight: "350px", zIndex: "-1 !important", marginTop :"80px"}} src="/www.skillplusplus.com.jpg" />
+        <Image
+          style={{
+            maxHeight: "350px",
+            zIndex: "-1 !important",
+            marginTop: !ContextState.isLoggedIn ? "140px" : "85px"
+          }}
+          src="/www.skillplusplus.com.jpg" />
       </Row>
     </Container>
   );
 }
-    
