83e167d771077f98b84852231d5700a85d2dcea4
Krit <jasada.k04@gmail.com>
2024-03-06 04:42:01 +0700

    style(cartPage):  purchase button

--- client/src/components/student/course-detail-page/course-detail.body.js
+++ client/src/components/student/course-detail-page/course-detail.body.js
@@ -5,6 +5,8 @@ import { useParams, useNavigate } from "react-router-dom";
 
 import ax from "../../../utils/config/ax";
 import conf from "../../../utils/config/main";
+import "./purchaseButton.css"
+
 
 export default function CourseDetailBody({
   course,
@@ -55,7 +57,7 @@ export default function CourseDetailBody({
   return (
     <>
       {chapters && course && (
-        <Card style={{ height: "800px", width: "auto", float: "center", backgroundColor:"white",}}>
+        <Card style={{ height: "800px", width: "auto", float: "center", backgroundColor: "white", }}>
           <Card.Body>
             <Row className="my-3">
               <Col>
@@ -109,11 +111,6 @@ export default function CourseDetailBody({
                   <div className="my-3">
                     {state.isLoggedIn && isPurchased ? (
                       <Button
-                        style={{
-                          width: "100%",
-                          backgroundColor: "blue",
-                          color: "white",
-                        }}
                         variant="secondary"
                         onClick={() => navigate(`/course/${courseName}/study`)}
                       >
@@ -121,27 +118,23 @@ export default function CourseDetailBody({
                       </Button>
                     ) : isCarted ? (
                       <Button
-                        style={{
-                          width: "100%",
-                          backgroundColor: "#B0D9B1",
-                          color: "#198754",
-                        }}
                         variant="secondary"
                         onClick={() => navigate("/cart")}
                       >
                         <span>ชำระเงิน</span>
                       </Button>
                     ) : (
-                      <Button className="purchaseButton"
-                        style={{
-                          width: "100%",
-                          backgroundColor: "#B0D9B1",
-                          color: "#198754",
-                        }}
-                        onClick={handleAddCart}
-                      >
-                        เพิ่มลงตะกร้า
-                      </Button>
+                      <div className="purchaseButton" onClick={handleAddCart}>
+                        <div class="button-wrapper">
+                          <div class="text" >Buy Now</div>
+                          <span class="icon">
+                            <svg viewBox="0 0 16 16" class="bi bi-cart2" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+                              <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
+                            </svg>
+                          </span>
+                        </div>
+                      </div>
+
                     )}
                   </div>
                 </Col>
