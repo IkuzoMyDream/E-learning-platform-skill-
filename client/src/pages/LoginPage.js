b5314ec7bb065b03be228ca5378b2dbd943ef1ba
เนติวุฒิ เกตุกำพล <6610110542@psu.ac.th>
2024-03-05 21:56:59 +0700

    edit loginpage

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,5 +1,7 @@
 import React, { useState, useContext } from "react";
 import { AuthContext } from "../utils/auth/Auth.context.js";
+import { Container } from "react-bootstrap";
+import { Link,useNavigate } from "react-router-dom";
 import './Csssignin.css';
 
 const initialState = {
@@ -11,12 +13,16 @@ function LoginPage() {
   const { state: ContextState, login } = useContext(AuthContext);
   const { isLoginPending, isLoggedIn, loginError } = ContextState;
   const [state, setState] = useState(initialState);
+  const navigate = useNavigate();
 
   const onSubmit = (e) => {
     e.preventDefault();
     const { username, password } = state;
     login(username, password);
     setState(initialState);
+    if (isLoggedIn) {
+      navigate("/");
+    }
   };
 
   const handleChange = (e) => {
@@ -28,37 +34,41 @@ function LoginPage() {
   };
 
   return (
-    <form className="form" onSubmit={onSubmit}>
-      <p className="form-title">Sign in to your account</p>
-      <div className="input-container">
-        <input
-          type="text"
-          placeholder="Enter email"
-          name="username"
-          onChange={handleChange}
-          value={state.username}
-        />
-      </div>
-      <div className="input-container">
-        <input
-          type="password"
-          placeholder="Enter password"
-          name="password"
-          onChange={handleChange}
-          value={state.password}
-        />
-      </div>
-      <button type="submit" className="submit">
-        Sign in
-      </button>
-      <p className="signup-link">
-        No account? <a href="">Sign up</a>
-      </p>
+    <Container className="d-flex justify-content-center align-items-center vh-100">
+        <form onSubmit={onSubmit}>
+          <div className="d-flex flex-column align-items-center">
+            <p className="form-title">Sign in to your account</p>
+            <div className="input-container">
+              <input
+                type="email"
+                placeholder="Enter email"
+                name="username"
+                onChange={handleChange}
+                value={state.username}
+              />
+            </div>
+            <div className="input-container">
+              <input
+                type="password"
+                placeholder="Enter password"
+                name="password"
+                onChange={handleChange}
+                value={state.password}
+              />
+            </div>
+            <button type="submit" className="submit">
+              Sign in
+            </button>
+            <p className="signup-link">
+              No account? <a href="">Sign up</a>
+            </p>
 
-      {isLoginPending && <div>Please wait...</div>}
-      {isLoggedIn && <div>Success.</div>}
-      {loginError && <div>{loginError.message}</div>}
-    </form>
+            {isLoginPending && <div>Please wait...</div>}
+            {isLoggedIn && <div>Success.</div>}
+            {loginError && <div>{loginError.message}</div>}
+          </div>
+        </form>
+    </Container>
   );
 }
 
